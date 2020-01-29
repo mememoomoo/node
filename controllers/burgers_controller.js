f2990f22303b4a7d3e9aa5c58ef801b763bdac28
Jack Larsen <jackrichardlarsen@gmail.com>
2020-01-28 20:42:44 -0700

    Fixing routing for heroku

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,29 +3,31 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-  res.redirect("/main");
-});
-
-router.get("*", function(req, res) {
-  res.redirect("/main");
-});
-
-router.get("/main", function(req, res) {
   burger.all(function(burger_data) {
     res.render("index", { burger_data });
   })
 });
 
+router.get("*", function(req, res) {
+  res.redirect("/");
+});
+
+// router.get("/main", function(req, res) {
+//   burger.all(function(burger_data) {
+//     res.render("index", { burger_data });
+//   })
+// });
+
 router.put("/burgers/update", function(req, res){
   burger.update(req.body.burger_id, function(r){
     console.log(r);
-    res.redirect("/main");
+    res.redirect("/");
   })
 });
 
 router.post("/burgers/create", function(req, res){
   burger.create(req.body.burger_name, function(r){
-    res.redirect("/main");
+    res.redirect("/");
   })
 })
 
