939462ede95ea425163b361becebb001ff90c819
Jack Larsen <jackrichardlarsen@gmail.com>
2020-01-28 21:02:54 -0700

    Fixing connection issues with heroku

--- config/orm.js
+++ config/orm.js
@@ -1,13 +1,5 @@
 var connection = require("./connections");
 
-function makeQuestionMarksBecauseSqlIsDumb(val) {
-    let array = [];
-    for (let i = 0; i < val; i++) {
-        array.push("?");     
-    }
-    return array.toString();
-}
-
 const orm = {
     all: function(tableInput, cb){
         connection.query("SELECT * FROM " + tableInput + ";", function(err, res) {
